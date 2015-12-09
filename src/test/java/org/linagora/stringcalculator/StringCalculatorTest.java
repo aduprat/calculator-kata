d3e2495b111e9eed31395765eb8b0af0b42b8bf9
Antoine Duprat <antduprat@gmail.com>
2015-12-09 12:09:27 +0100

    Test reference

--- src/test/java/org/linagora/stringcalculator/StringCalculatorTest.java
+++ src/test/java/org/linagora/stringcalculator/StringCalculatorTest.java
@@ -1,5 +1,10 @@
 package org.linagora.stringcalculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
     /**
@@ -11,4 +16,51 @@ public class StringCalculatorTest {
     *    6. Negative numbers throw an exception ("3,4\-2" -> Exception)
     *    7. Numbers greater than 1000 are ignored ("3,4\n1002" -> 7)
     **/
+
+    private StringCalculator testee;
+
+    @Before
+    public void setup() {
+        testee = new StringCalculator();
+    }
+
+    @Test
+    public void calculateShouldReturnZeroWhenEmptyString() {
+        assertThat(testee.calculate("")).isEqualTo(0);
+    }
+
+    @Test
+    public void calculateShouldReturnZeroWhenNullString() {
+        assertThat(testee.calculate(null)).isEqualTo(0);
+    }
+
+    @Test
+    public void calculateShouldReturnTheValueWhenSingleNumber() {
+        assertThat(testee.calculate("3")).isEqualTo(3);
+    }
+
+    @Test
+    public void calculateShouldReturnTheSumWhenTwoNumbersCommaDelimited() {
+        assertThat(testee.calculate("3,4")).isEqualTo(7);
+    }
+
+    @Test
+    public void calculateShouldReturnTheSumWhenTwoNumbersNewlineDelimited() {
+        assertThat(testee.calculate("3\n4")).isEqualTo(7);
+    }
+
+    @Test
+    public void calculateShouldReturnTheSumWhenThreeNumbersMixedDelimited() {
+        assertThat(testee.calculate("3,4\n2")).isEqualTo(9);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void calculateShouldThrowAnExceptionWhenNegativeNumber() {
+        testee.calculate("3,4\n-2");
+    }
+
+    @Test
+    public void calculateShouldIgnoreNumberWhenGreaterThan1000() {
+        assertThat(testee.calculate("3,4\n1002")).isEqualTo(7);
+    }
 }
